452a843854ba5642c28e02170f8f3df49e8db3b7
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-11 15:02:08 +0700

    add file

--- 2.sh
+++ 2.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo "just input some word to the terminal"
+
+echo "this is script number $0"
+echo "the first word is $1"
+echo "the second word is $2"
+echo "the third word is $3"
+
+
