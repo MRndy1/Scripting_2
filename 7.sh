3240529e73dbab13763b079164f908e78c8e2cac
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-12 12:46:41 +0700

    simple or ||

--- 7.sh
+++ 7.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+echo -e "please input one of my dog name: \c"
+read dog
+a=heli
+b=anti
+
+
+if [[ $dog == $a || $dog == $b ]]
+then
+    echo "you were right my dog is $dog"
+else
+    echo "nah false"
+fi
