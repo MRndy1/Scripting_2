1a55e86ae2afb2c0e1cf9f4ef0a7c08e4c94ea9c
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-12 10:49:42 +0700

    adding some notes

--- 4.sh
+++ 4.sh
@@ -7,7 +7,7 @@ echo -e "Enter the name of the file : \c"
 # -e the flag for interpretae \c, || \c for keep read on the same line
 read file
 
-if [ -e $file ]
+if [ -s $file ]
 then
     echo "this $file exist"
 else
@@ -15,3 +15,14 @@ else
 fi
 
 pwd Desktop/$file
+
+: '
+-e if the file exist or not
+-f if the file exist and regular file or not
+-d directory 
+-b block special file || like music vid 
+-c character special file
+-s check if the file empty or not
+-rwx check if the file have the rwx permission
+
+'
