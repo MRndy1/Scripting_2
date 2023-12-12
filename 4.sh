a1ce177f1385f004cd574917a91099566f0b8abd
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-12 10:42:11 +0700

    if for finding a name of a file

--- 4.sh
+++ 4.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+cd
+cd Desktop
+
+echo -e "Enter the name of the file : \c"
+# -e the flag for interpretae \c, || \c for keep read on the same line
+read file
+
+if [ -e $file ]
+then
+    echo "this $file exist"
+else
+    echo "this $file is not exist"
+fi
+
+pwd Desktop/$file
