257b215ab153a838e2cb553c9a19208e9a90da0c
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-12 11:29:10 +0700

    append a text with if statement

--- 5.sh
+++ 5.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+#!/bin/bash
+
+echo -e "Enter the file name : \c"
+read file
+
+if [ -f $file ]
+then
+    if [ -w $file ]
+    then
+        echo "please input some word"
+        echo "after input the data press ctr + c"
+        cat >> $file
+    else
+        echo "the file doesnt have the write permission"
+    fi
+else
+    ehco "the file is not exist"
+fi
