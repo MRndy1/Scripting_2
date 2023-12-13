80d3c59551e79fab286d6423c5d499a984598d68
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-13 11:54:05 +0700

    first uses while loops

--- 12.sh
+++ 12.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+#the while loops
+
+: ' 
+
+while [ condition]
+do
+    command1
+    command1
+    command1
+done
+
+'
+echo -e "please pick a number between 0-20 : \c"
+read num
+
+while (( $num <= 20 ))
+do
+    echo $num
+    (( num++ ))
+done
