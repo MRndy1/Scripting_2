852c5919e97fafd6258f7721642bd9f8b6aa0678
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-13 12:01:41 +0700

    the while loops

--- 12.sh
+++ 12.sh
@@ -15,8 +15,10 @@ done
 echo -e "please pick a number between 0-20 : \c"
 read num
 
-while (( $num <= 20 ))
+while (( $num <= 9 ))
 do
-    echo $num
+    #echo $num
     (( num++ ))
+    #sleep 1 || adding a break for 1 second
+    notepad
 done
