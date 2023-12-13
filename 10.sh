d8bcf638de0ea5166aff375278980dbb1d61d78e
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-13 10:23:50 +0700

    first case statement

--- 10.sh
+++ 10.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 #case statement
 
+: ' 
+case expression in
+    pattern1 )
+        statements ;;
+    pattern2 )
+        statements ;;
+    ...
+esac
 
+'
+echo "Hello, what vehicle do you want to rent"
+read veh
+
+case $veh in
+    "10" )
+        echo "yes this is a 10" ;;
+    "van" )
+        echo "Rent of $veh is 110 dollar" ;;
+    "bicycle" )
+        echo "Rent of $veh is 10 dollar" ;;
+    "truck" )
+        echo "Rent of $veh is 150 dollar" ;; 
+    * )
+        echo "unknown vehicle" ;;
+esac
