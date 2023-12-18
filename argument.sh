827b122f9006528d3fc4e5a47202ab3b4c50b106
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2023-12-18 14:25:28 +0700

    argument

---
+++ argument.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+lines=$(ls -lh $1 | wc -l)
+
+if (( $# != 1 ))
+then
+    echo "this script requires exactly one directory path passed to it."
+    echo "please try again"
+else
+    echo "you have $lines in $1 directory"
+fi
